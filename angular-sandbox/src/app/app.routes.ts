ae7284ee18af227bc46364a50c8a6d8c3c9238b3
Thomas Dijkmans <tdijkmans@gmail.com>
2025-07-26 12:35:18 +0200

    Remove unused drawer component and related files; clean up app structure

--- angular-sandbox/src/app/app.routes.ts
+++ angular-sandbox/src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { ZaakProfiel } from './zaakprofiel/zaak-profiel.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/zaakprofiel/3', pathMatch: 'full' },
-    {path: 'zaakprofiel/:zaakid', component: ZaakProfiel}
 ];
